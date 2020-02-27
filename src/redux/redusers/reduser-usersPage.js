a785b43cc47b384288f6f329c1733e1f9a0274b5
Alexandr Smirnov <alexandersmirnov2014@mail.ru>
2020-02-28 00:31:07 +0300

    Initial state has become empty. Side effect inside Users-Component

--- src/redux/redusers/reduser-usersPage.js
+++ src/redux/redusers/reduser-usersPage.js
@@ -3,41 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
 let initialState = {
-    users: [
-        {
-            id: 1,
-            photoURL: 'https://ru.baku.ws/uploads/posts/2019-06/1559909852_vcr8zt7l8bg.jpg',
-            followed: true,
-            fullName: 'Azar J.',
-            status: `I log in here`,
-            location: {
-                cityName: 'London',
-                contry: 'England',
-            },
-        },
-        {
-            id: 2,
-            photoURL: 'https://realworldofsport.files.wordpress.com/2015/08/c5e01-john2bterry.jpg?w=1200',
-            followed: false,
-            fullName: 'Terry M.',
-            status: `I log in here`,
-            location: {
-                cityName: 'London',
-                contry: 'England',
-            },
-        },
-        {
-            id: 3,
-            photoURL: 'https://img.tsn.ua/cached/1533897632/tsn-d5a3c83b3e2b3f0a9ff57401c8651a3a/thumbs/1200x630/58/7a/47651445fd9362215813fa3c8ff57a58.jpg',
-            followed: false,
-            fullName: 'Lampard F.',
-            status: `I log in here`,
-            location: {
-                cityName: 'London',
-                contry: 'England',
-            },
-        },
-    ]
+    users: []
 }
 
 const reducerUsersPage = (state = initialState, action) => {
