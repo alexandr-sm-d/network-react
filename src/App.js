20b09194afbff9d996cf5eae5281c0d0cbe79e1f
Alexandr Smirnov <alexandersmirnov2014@mail.ru>
2020-03-13 20:40:33 +0300

    before fix bug :userID?

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Navigation />
         <div class="content-container">
           <main className='content'>
-            <Route path='/profile' render={() => <ProfileContainer />} />
+            <Route path='/profile/:userID?' render={() => <ProfileContainer />} />
             <Route path='/users' render={() => <UsersContainer />} />
             <Route path='/dialogs' render={() => <DialogsContainer />} />
             <Route path='/newsLenta' component={News} />
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,26 +3,27 @@ import Profile from './Profile';
 import * as axios from 'axios';
 import { connect } from "react-redux";
 import { setUserProfile } from '../../redux/redusers/reduser-profilePage';
+import { withRouter } from 'react-router-dom';
 
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
 })
 
 class ProfileContainer extends React.Component {
-    // debugger;
     componentDidMount() {
+        debugger;
+        let uID = this.props.match.params.userID;
+
         axios.get(
-            `https://social-network.samuraijs.com/api/1.0/profile/2`
+            `https://social-network.samuraijs.com/api/1.0/profile/` + uID
         )
             .then(resp => {
-                // debugger;
                 this.props.setUserProfile(resp.data);
             })
     }
 
     render() {
         // debugger;
-        // console.log(this.props)
         return (
             <Profile {...this.props} profile={this.props.profile}/>
         )
@@ -30,4 +31,7 @@ class ProfileContainer extends React.Component {
 
 }
 
-export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);
+let WithRouterProfileContainer = withRouter(ProfileContainer)
+
+
+export default connect(mapStateToProps, {setUserProfile})(WithRouterProfileContainer);
