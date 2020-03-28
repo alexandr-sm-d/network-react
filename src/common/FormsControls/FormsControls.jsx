6e9c6848e7d1a63151564bb88d07b6fcd1dc9d77
Alexandr Smirnov <alexandersmirnov2014@mail.ru>
2020-03-29 01:11:43 +0300

    validation for profile post is working

--- src/common/FormsControls/FormsControls.jsx
+++ src/common/FormsControls/FormsControls.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import classesStyle from './FormsControls.module.css'
 
-export const Textarea = props => {
+export const Textarea = ({ input, meta, ...props }) => {
     // debugger
+    const hasError = meta.touched && meta.error;
     return (
-        <div>
-            <textarea {...props.input} {...props}/>>
+        <div className={`${classesStyle.form} ${hasError ? classesStyle.error : ''}`}>
+            <div>
+                <textarea className={classesStyle.textarea} {...input} {...props} />
+            </div>
+            {hasError && <span>{meta.error}</span>}
         </div>
     )
 }
