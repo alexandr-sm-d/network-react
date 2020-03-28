6431789a348f2f04def2b943979083738fc76788
Alexandr Smirnov <alexandersmirnov2014@mail.ru>
2020-03-29 02:07:32 +0300

    validation for messages is working

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,12 +3,15 @@ import classesStyle from './Dialogs.module.css'
 import DialogItem from './DialogsItem/DialogsItem';
 import MessageItem from './MessageItem/MessageItem';
 import { reduxForm, Field } from 'redux-form';
+import { Textarea } from '../../common/FormsControls/FormsControls';
+import { validatorMaxLength, validatorEmptyPost } from '../../utlis/validators/validatorsProfile';
+
+const maxLength50 = validatorMaxLength(50);
 
 const Dialogs = (props) => {
 
     let dialogs = props.dialogsPage.dialogsData.map((user) => <DialogItem name={user.name} id={user.id} />);
     let messages = props.dialogsPage.messagesData.map((mes) => <MessageItem message={mes.message} />);
-    let newText = props.dialogsPage.newMessageText
 
     function sendMessage(values) {
         props.sendNewMessage(values.messageForSubmit)
@@ -29,11 +32,12 @@ const Dialogs = (props) => {
 }
 
 const DialogsForm = (props) => {
-
+    
     return (
         <form onSubmit={props.handleSubmit}>
             <Field name="messageForSubmit"
-                component="textarea"
+                component={Textarea}
+                validate={[validatorEmptyPost, maxLength50]}
                 placeholder='Enter your message' />
             <div>
                 <button>Send Message</button>
