2b7cb96060b73a579fd8c90e21a42cfb2fc7b803
Alexandr Smirnov <alexandersmirnov2014@mail.ru>
2020-04-13 23:03:08 +0300

    refactoring user component

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,17 +1,47 @@
+import React from 'react';
+import Users from './Users';
+import Preloader from '../../common/preloader/Preloader';
 import { connect } from "react-redux";
-import UsersAPIComponent from "./UsersAPIComponent";
-import { follow, unfollow,
-    setCurrentPage, requestUsers, } from "../../redux/reducers/usersPage-reducer";
-import { getUsers, getPageSize, getTotalCountUsers, getCurrentPage, getIsFetching, getFollowingInProgress } from "../../redux/reducers/usersPage-selectors";
+import {
+    follow, unfollow,
+    setCurrentPage, requestUsers,
+} from "../../redux/reducers/usersPage-reducer";
+import {
+    getUsers, getPageSize,
+    getTotalCountUsers, getCurrentPage,
+    getIsFetching, getFollowingInProgress
+} from "../../redux/reducers/usersPage-selectors";
 
-// const mapStateToProps = (state) => ({
-//     users: state.usersPage.users,
-//     pageSize: state.usersPage.pageSize,
-//     totalCountUsers: state.usersPage.totalCountUsers,
-//     currentPage: state.usersPage.currentPage,
-//     isFetching: state.usersPage.isFetching,
-//     followingInProgress: state.usersPage.followingInProgress,
-// })
+// UsersAPIComponent - промежуточная контейнерная компонента, которая оборачивает Users.
+class UsersAPIComponent extends React.Component {
+
+    componentDidMount() {
+        this.props.requestUsers(this.props.currentPage, this.props.pageSize)
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.requestUsers(pageNumber, this.props.pageSize)
+    }
+
+    render() {
+        return (
+            <>
+                {this.props.isFetching ? <Preloader /> : null}
+                <Users
+                    totalCountUsers={this.props.totalCountUsers}
+                    pageSize={this.props.pageSize}
+                    currentPage={this.props.currentPage}
+                    onPageChanged={this.onPageChanged}
+                    users={this.props.users}
+                    follow={this.props.follow}
+                    unfollow={this.props.unfollow}
+                    followingInProgress={this.props.followingInProgress}
+                    toggleFollowing={this.props.toggleFollowing}
+                />
+            </>
+        )
+    }
+}
 
 const mapStateToProps = (state) => ({
     users: getUsers(state),
