129f05d263fe45a2566521f49f9be09f1701581b
Alexandr Smirnov <alexsm.dev@gmail.com>
2020-04-29 15:59:52 +0300

    DEV: added pages. Refactoring paginator

--- src/components/Dev/Dev.jsx
+++ src/components/Dev/Dev.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Counter from './Counter';
 import Dialog from './Dialog';
+import Pages from './Pages';
 
 const Dev = props => {
+
     return (
         <div>
             <h1>Developer window</h1>
             <Dialog />
             {/* <Counter /> */}
+            <Pages />
         </div>
     )
 }
---
+++ src/components/Dev/Pages.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import classesStyle from './Pages.module.css'
+
+const Pages = props => {
+    
+    let n = 187;
+    let pages = [];
+    for (let i = 1; i < n; i++) {
+        pages.push(i)
+    }
+    let leftBorderPage = 11;
+    let rightBorderPage = 20;
+
+    return (
+        <div className={classesStyle.pages}>
+            {pages
+            .filter(page => page >= leftBorderPage && page <= rightBorderPage)
+            .map(page => <span className={classesStyle.page}>{page}</span>)}
+            <div>
+                <button>back</button>
+                <button>next</button>
+            </div>
+        </div>
+    )
+}
+
+export default Pages;
