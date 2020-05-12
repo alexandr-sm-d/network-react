32052eb9de63fcc980e2bd603057112ed587138b
Alexandr Smirnov <alexsm.dev@gmail.com>
2020-05-12 14:26:02 +0300

    layout implemented

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React, { Suspense } from 'react';
-import './App.css';
+import React, {Suspense} from 'react';
+// import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import News from './components/News/News';
 import Music from './components/Music/Music';
-import { Route, withRouter } from 'react-router-dom';
+import {Route, withRouter} from 'react-router-dom';
 import Settings from './components/Settings/Settings';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
-import { getAuthUserDataTC } from './redux/reducers/auth-reducer';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import { intitializingTC } from './redux/reducers/app-reducer';
+import {getAuthUserDataTC} from './redux/reducers/auth-reducer';
+import {compose} from 'redux';
+import {connect} from 'react-redux';
+import {intitializingTC} from './redux/reducers/app-reducer';
 import Preloader from './common/preloader/Preloader';
 import 'antd/dist/antd.css'
+import {Layout} from "antd";
+import classesStyle from "./AppAntd.module.css";
 
+const {Header, Footer, Sider, Content} = Layout;
 const Dev = React.lazy(() => import('./components/Dev/Dev'))
 
 class App extends React.Component {
@@ -29,37 +32,73 @@ class App extends React.Component {
         {
             if (!this.props.appIsReady) {
                 return (
-                    <Preloader />
+                    <Preloader/>
                 )
             }
         }
 
-        return (
-            <div className='wrapper'>
-                <div className='container'>
-                    <HeaderContainer />
-                    <Navigation />
-                    <div className="content-container">
-                        <main className='content'>
-                            <Route path='/profile/:userID?' render={() => <ProfileContainer />} />
-                            <Route path='/users' render={() => <UsersContainer />} />
-                            <Route path='/dialogs' render={() => <DialogsContainer />} />
-                            <Route path='/musicList' component={Music} />
-                            <Route path='/settingsApp' component={Settings} />
-                            <Route path='/login' component={Login} />
-                            <Route path='/dev' render={() => 
-                                <Suspense fallback={<div>Loading...</div>}>
-                                    <Dev />
-                                </Suspense>
-                            } />
-                        </main>
-                    </div>
-                    <footer className='foot'>
+        // return (
+        //     <div className='wrapper'>
+        //         <div className='container'>
+        //             <HeaderContainer />
+        //             <Navigation />
+        //             <div className="content-container">
+        //                 <main className='content'>
+        //                     <Route path='/profile/:userID?' render={() => <ProfileContainer />} />
+        //                     <Route path='/users' render={() => <UsersContainer />} />
+        //                     <Route path='/dialogs' render={() => <DialogsContainer />} />
+        //                     <Route path='/musicList' component={Music} />
+        //                     <Route path='/settingsApp' component={Settings} />
+        //                     <Route path='/login' component={Login} />
+        //                     <Route path='/dev' render={() =>
+        //                         <Suspense fallback={<div>Loading...</div>}>
+        //                             <Dev />
+        //                         </Suspense>
+        //                     } />
+        //                 </main>
+        //             </div>
+        //             <footer className='foot'>
+        //
+        //             </footer>
+        //         </div>
+        //     </div>
+        // );
 
-                    </footer>
+        return (
+            <div className={classesStyle.container}>
+                <div style={{width: '60%'}}>
+                    <Layout className={classesStyle.layout}>
+                        <Header className={classesStyle.headerAtdn}>
+                            <HeaderContainer/>
+                        </Header>
+
+                        <Layout>
+                            <Sider className={classesStyle.navigationAtdn}>
+                                <Navigation/>
+                            </Sider>
+
+                            <Content className={classesStyle.contentAtdn}>
+                                <main>
+                                    <Route path='/profile/:userID?' render={() => <ProfileContainer/>}/>
+                                    <Route path='/users' render={() => <UsersContainer/>}/>
+                                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                                    <Route path='/musicList' component={Music}/>
+                                    <Route path='/settingsApp' component={Settings}/>
+                                    <Route path='/login' component={Login}/>
+                                    <Route path='/dev' render={() =>
+                                        <Suspense fallback={<div>Loading...</div>}>
+                                            <Dev/>
+                                        </Suspense>
+                                    }/>
+                                </main>
+                            </Content>
+                        </Layout>
+
+                        <Footer className={classesStyle.foot}>Footer</Footer>
+                    </Layout>
                 </div>
             </div>
-        );
+        )
     }
 }
 
--- src/components/Dev/Dev.jsx
+++ src/components/Dev/Dev.jsx
@@ -4,9 +4,11 @@ import Dialog from './Dialog';
 import Pages from './Pages';
 import ChoiseFile from './ChoiseFile';
 import UseCallback from "./useCallback/UseCallback";
-import {Button} from "antd";
+import {Button, Layout} from "antd";
 import classesStyle from './Dev.module.css';
 
+
+const { Header, Footer, Sider, Content } = Layout;
 const Dev = props => {
 
     return (
@@ -21,6 +23,14 @@ const Dev = props => {
             <Button className={classesStyle.default}>Default</Button>
             <Button type="dashed">Dashed</Button>
             <Button type="link">Link</Button>
+            <Layout className={classesStyle.layout}>
+                <Header>Header</Header>
+                <Layout>
+                    <Sider>Sider</Sider>
+                    <Content>Content</Content>
+                </Layout>
+                <Footer className={classesStyle.foot}>Footer</Footer>
+            </Layout>
         </div>
     )
 }
