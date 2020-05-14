e9dbd2e22ae89374fe2e4685582902fc626b50f4
Alexandr Smirnov <alexsm.dev@gmail.com>
2020-05-14 09:19:31 +0300

    improved sidebar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,27 +1,53 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import classesStyle from './Navigation.module.css';
+import {
+    ProfileOutlined,
+    TeamOutlined,
+    MessageOutlined,
+    SettingOutlined,
+    BellOutlined,
+    RocketOutlined
+} from "@ant-design/icons";
 
 const Navigation = () => {
     return (
         <nav className={classesStyle.nav}>
             <div className={classesStyle.item}>
-                <NavLink to='/profile' activeClassName={classesStyle.activeLink}>Profile</NavLink>
+                <NavLink to='/profile' activeClassName={classesStyle.activeLink}>
+                    <ProfileOutlined className={classesStyle.navigationIcon}/>
+                    Profile
+                </NavLink>
             </div>
             <div className={classesStyle.item}>
-                <NavLink to='/users' activeClassName={classesStyle.activeLink}>Users</NavLink>
+                <NavLink to='/users' activeClassName={classesStyle.activeLink}>
+                    <TeamOutlined />
+                    Users
+                </NavLink>
             </div>
             <div className={classesStyle.item}>
-                <NavLink to='/dialogs' activeClassName={classesStyle.activeLink}>Message</NavLink>
+                <NavLink to='/dialogs' activeClassName={classesStyle.activeLink}>
+                    <MessageOutlined />
+                    Message
+                </NavLink>
             </div>
             <div className={classesStyle.item}>
-                <NavLink to='/musicList' activeClassName={classesStyle.activeLink}>Music</NavLink>
+                <NavLink to='/musicList' activeClassName={classesStyle.activeLink}>
+                    <BellOutlined />
+                    Music
+                </NavLink>
             </div>
             <div className={classesStyle.item}>
-                <NavLink to='/settingsApp' activeClassName={classesStyle.activeLink}>Settings</NavLink>
+                <NavLink to='/settingsApp' activeClassName={classesStyle.activeLink}>
+                    <SettingOutlined />
+                    Settings
+                </NavLink>
             </div>
             <div className={classesStyle.item}>
-                <NavLink to='/dev' activeClassName={classesStyle.activeLink}>Dev</NavLink>
+                <NavLink to='/dev' activeClassName={classesStyle.activeLink}>
+                    <RocketOutlined />
+                    Dev
+                </NavLink>
             </div>
         </nav>
     )
