ac3c6b338b5580486a1ec16a26675c7b411a4992
Alexandr Smirnov <alexsm.dev@gmail.com>
2020-06-05 15:53:46 +0300

    users improved (css)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -9,12 +9,14 @@ const Users = (props) => {
 
     return (
         <div className={classesStyle.usersPage}>
+
             {/*<Paginator*/}
             {/*    totalCountUsers={props.totalCountUsers}*/}
             {/*    pageSize={props.pageSize}*/}
             {/*    currentPage={props.currentPage}*/}
             {/*    onPageChanged={props.onPageChanged}*/}
             {/*/>*/}
+
             <Pagination
                 className={classesStyle.pagination}
                 onChange={props.onPageChanged}
@@ -25,25 +27,35 @@ const Users = (props) => {
 
             {props.users.map((u) => (
                 <div className={classesStyle.user}>
-                    <NavLink to={'/profile/' + u.id}>
-                        <img src={
-                            u.photos.small != null
-                                ? u.photos.small
-                                : userPhoto
-                        }/>
-                    </NavLink>
-                    <div> {u.name} </div>
-                    <div> {'u.location.cityName'} </div>
-                    <div> {'u.location.contry'} </div>
-                    {u.followed
-                        ?
-                        <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                            props.unfollow(u.id);
-                        }}>unfollow</button>
-                        :
-                        <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                            props.follow(u.id);
-                        }}>follow</button>}
+                    <div className={classesStyle.photo}>
+                        <NavLink to={'/profile/' + u.id}>
+                            <img src={
+                                u.photos.large != null
+                                    ? u.photos.large
+                                    : userPhoto
+                            }/>
+                        </NavLink>
+                        {u.followed
+                            ?
+                            <button
+                                className={classesStyle.follow}
+                                disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
+                                props.unfollow(u.id);
+                            }}>unfollow</button>
+                            :
+                            <button
+                                className={classesStyle.unfollow}
+                                disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
+                                props.follow(u.id);
+                            }}>follow</button>}
+                    </div>
+                    <div className={classesStyle.info}>
+                        <div>
+                            <b>{u.name}</b>
+                        </div>
+                        <div> {'u.location.cityName'} </div>
+                        <div> {'u.location.contry'} </div>
+                    </div>
                 </div>
             ))}
         </div>
