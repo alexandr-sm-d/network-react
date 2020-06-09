ce306bf2901acbba5abcc1a872e0c12ba40b5684
Alexandr Smirnov <alexsm.dev@gmail.com>
2020-06-09 15:42:35 +0300

    settings improved (css)

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -4,7 +4,7 @@ import classesStyle from './Settings.module.css';
 const Settings = () => {
     return (
         <div className={classesStyle.container}>
-            Settings App
+            Settings of App
         </div>
     )
 }
